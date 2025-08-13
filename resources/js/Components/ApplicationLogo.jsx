1294a89390e4a6a04fee6459ec054ae88f902d37
Ajsal <Ajsal@EXPSHARJAHSRV>
2025-08-13 16:59:21 +0400

    Home screen setup with default browser theme

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,7 @@
 export default function ApplicationLogo(props) {
     return (
         <img
-            src="/assets/images/ChatBuzzLogo-2.svg"
+            src="/assets/images/ChatBuzzLogo-4.png"
             alt="ChatBuzz Logo"
             className="w-20 h-20"
         />
